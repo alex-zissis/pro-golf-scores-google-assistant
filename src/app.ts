4e9addfe8b343750de9f6547f435b21c2345ca11
Alex Zissis <azissis@campaignmonitor.com>
2022-01-10 20:48:47 +1100

    Fix route typo

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 app.post('/', golf);
 
-app.get('/heath', (req, res) => res.status(200).send('👍'));
+app.get('/health', (req, res) => res.status(200).send('👍'));
 
 const port = 3000;
 app.listen(3000, () => console.log(`listening on port ${port}`));
